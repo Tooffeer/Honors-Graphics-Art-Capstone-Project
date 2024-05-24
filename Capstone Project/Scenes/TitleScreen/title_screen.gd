a33eec9cd7d1082240fbe5a32704eb117003f41c
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2024-05-24 11:30:46 -0700

    Fullscreen Button

--- Capstone Project/Scenes/TitleScreen/title_screen.gd
+++ Capstone Project/Scenes/TitleScreen/title_screen.gd
@@ -9,3 +9,11 @@ func _on_start_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _process(_delta):
+	if $Options/Fullscreen.button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	print(DisplayServer.window_get_size())
