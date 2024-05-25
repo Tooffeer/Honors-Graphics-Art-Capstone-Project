3adca740ee5eb65ba986cb1ea3cc794a2dba894e
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2024-05-25 00:33:38 -0700

    Fixed Windows Error
    cannot constantly set window mode, breaks that shit on windows. For future reference make sure my code works on both mac and windows.

--- Capstone Project/Scenes/TitleScreen/title_screen.gd
+++ Capstone Project/Scenes/TitleScreen/title_screen.gd
@@ -11,9 +11,10 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _process(_delta):
-	if $Options/Fullscreen.button_pressed:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	pass
+#	if $Options/Fullscreen.button_pressed:
+#		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+#	else:
+#		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	
 	#print(DisplayServer.window_get_size())
