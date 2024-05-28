c9cf23ad219dab000a8e5693d3ebbda3e6acc24c
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2024-05-27 18:30:20 -0700

    Save

--- Capstone Project/Scenes/TitleScreen/title_screen.gd
+++ Capstone Project/Scenes/TitleScreen/title_screen.gd
@@ -6,15 +6,15 @@ func _ready():
 func _on_start_pressed():
 	Global.emit_signal("startGame")
 	visible = false
+	 
+
+func _on_fullscreen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	print(DisplayServer.window_get_size())
 
 func _on_quit_pressed():
 	get_tree().quit()
-
-func _process(_delta):
-	pass
-	#if $Options/Fullscreen.button_pressed:
-	#	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	#else:
-	#	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	
-	#print(DisplayServer.window_get_size())
